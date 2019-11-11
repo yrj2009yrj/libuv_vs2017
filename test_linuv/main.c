4034bc4c6588ce93889c24291bc865ffab89c293
yurongjun <yrj2009yrj@qq.com>
2019-11-11 21:37:49 +0800

    three

--- test_linuv/main.c
+++ test_linuv/main.c
@@ -1,24 +1,64 @@
+#include <assert.h>
 #include <stdio.h>
+#include <fcntl.h>
 #include <uv.h>
 
-int64_t counter = 0;
+void on_read(uv_fs_t *req);
 
-void wait_for_a_while(uv_idle_t* handle) {
-    counter++;
+uv_fs_t open_req;
+uv_fs_t read_req;
+uv_fs_t write_req;
 
-    if (counter >= 10e6)
-        uv_idle_stop(handle);
+static char buffer[1024];
+
+static uv_buf_t iov;
+
+void on_write(uv_fs_t *req) {
+    if (req->result < 0) {
+        fprintf(stderr, "Write error: %s\n", uv_strerror((int)req->result));
+    }
+    else {
+        uv_fs_read(uv_default_loop(), &read_req, open_req.result, &iov, 1, -1, on_read);
+    }
 }
 
-int main() {
-    uv_idle_t idler;
+void on_read(uv_fs_t *req) {
+    if (req->result < 0) {
+        fprintf(stderr, "Read error: %s\n", uv_strerror(req->result));
+    }
+    else if (req->result == 0) {
+        uv_fs_t close_req;
+        // synchronous
+        uv_fs_close(uv_default_loop(), &close_req, open_req.result, NULL);
+    }
+    else if (req->result > 0) {
+        iov.len = req->result;
+        uv_fs_write(uv_default_loop(), &write_req, 1, &iov, 1, -1, on_write);
+    }
+}
 
-    uv_idle_init(uv_default_loop(), &idler);
-    uv_idle_start(&idler, wait_for_a_while);
+void on_open(uv_fs_t *req) {
+    // The request passed to the callback is the same as the one the call setup
+    // function was passed.
+    assert(req == &open_req);
+    if (req->result >= 0) {
+        iov = uv_buf_init(buffer, sizeof(buffer));
+        uv_fs_read(uv_default_loop(), &read_req, req->result,
+            &iov, 1, -1, on_read);
+    }
+    else {
+        fprintf(stderr, "error opening file: %s\n", uv_strerror((int)req->result));
+    }
+}
 
-    printf("Idling...\n");
+int main(int argc, char **argv) {
+    uv_fs_open(uv_default_loop(), &open_req, argv[1], O_RDONLY, 0, on_open);
     uv_run(uv_default_loop(), UV_RUN_DEFAULT);
 
-    uv_loop_close(uv_default_loop());
+    uv_fs_req_cleanup(&open_req);
+    uv_fs_req_cleanup(&read_req);
+    uv_fs_req_cleanup(&write_req);
+
+    getchar();
     return 0;
 }
