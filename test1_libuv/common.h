95c19a63c578c15afb995f3fad1fdf29e3d0b73c
yurongjun <yrj2009yrj@qq.com>
2019-11-27 21:02:57 +0800

    20191127

---
+++ test1_libuv/common.h
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include "uv.h"
+
+#define CHECK(r, msg) if (r < 0) {                                                              \
+    fprintf(stderr, "%s: [%s(%d): %s]\n", msg, uv_err_name((r)), (int) r, uv_strerror((r)));    \
+    exit(1);                                                                                    \
+}
+
+typedef struct context_struct {
+    uv_fs_t *open_req;
+    uv_buf_t buf;
+} uv_context_t;
+
+typedef struct {
+    uv_write_t req;
+    uv_buf_t buf;
+} write_req_t;
+
+typedef struct {
+    char string[30];
+    int number;
+} thread_args_t;
